6672621c479b992a69c3cc37fdc54301027112d6
Caleb Harding <calebmharding@icloud.com>
2024-01-25 22:01:43 -0700

    Update link and article scraper, start mongodb operations

--- article_scraper.py
+++ article_scraper.py
@@ -1,13 +1,38 @@
 '''given a url, scrape the article information'''
 
+import json
+import openai
 from newspaper import Article
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 
+def process_complicated_author(author):
+
+    with open('./openai_auth.json', 'r') as json_file:
+        json_load = json.load(json_file)
+
+    openai.api_key = json_load["api_key"]
+
+    prompt = f"""
+    Please give me the names of the authors included in this text, and remove any unnecessary spaces within names. Please give me the names, seperated by a space, and nothing else. For example, if the text was "求是》杂志记者 何雯雯 新县融媒体中心记者 韩 燕", I would want you to tell me "何雯雯 韩燕". The text is: {author}
+    """
+
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[{
+            "role":"user",
+            "content":prompt
+        }]
+    )
+
+    return response.choices[0].message.content
+
+
 class QiuShiArticleParser:
     def __init__(self, url):
         # get the beautiful soup version
+        self.url = url
         page = urlopen(url)
         html = page.read().decode("utf-8")
         self.soup = BeautifulSoup(html, "html.parser")
@@ -17,17 +42,52 @@ class QiuShiArticleParser:
         self.article.download()
         self.article.parse()
 
-
-    def get_author(self):
+    def get_authors(self):
         span_els = self.soup.find_all("span", class_="appellation")
-        # author is the second appel element
-        author_el = span_els[1].text.strip()
-        # remove the "Author:" piece
-        author = author_el.split("：")[1]
-        return author
+        try:
+            # author is the second appel element
+            author_el = span_els[1].text.strip()
+            # remove the "Author:" piece
+            author = author_el.split("：")[1]
+
+            # see if it needs additional processing
+            # no spaces, should be good to go. Or if None, return
+            if author == None:
+                return [author]
+            elif " " not in author:
+                return [author]
+            # spaces, but length three - a 2 char name with a space
+            elif len(author) == 3:
+                return [author[0] + author[2]]
+            # something more complicated is going on - could be multiple authors, could have additional info 
+            else:
+                print("Calling ChatGPT...")
+                try:
+                    author = process_complicated_author(author)
+                    print(f"Author returned by ChatGPT: {author}")
+                    # split the result in case there is more than one author
+                    return author.split(" ")
+                except:
+                    print("ChatGPT call failed")
+                return "CHATGPT" + author
+
+        except:
+            return None
     
     def get_title(self):
         return self.soup.find('h1').text.strip()
+    
+    def get_year_edition(self):
+        span_els = self.soup.find_all("span", class_="appellation")
+        # year/edition is the first appel element
+        ye_text = span_els[0].text.strip()
+        # get the last piece with the date
+        cleaned_ye_text = ye_text[-7:]
+
+        year = cleaned_ye_text[:4]
+        edition = cleaned_ye_text[-2:]
+
+        return year, edition
     
     def get_date(self):
         date_el = self.soup.find("span", class_="pubtime").text.strip()
@@ -35,15 +95,25 @@ class QiuShiArticleParser:
         date = date_el.split(" ")[0]
         return date
     
+    def get_url(self):
+        return self.url
+    
     def get_text(self):
         return self.article.text.replace('\n', "")
     
 class QiuShiArticle:
     def __init__(self, article_parser):
-        self.author = article_parser.get_author()
+        self.authors = article_parser.get_authors()
         self.title = article_parser.get_title()
         self.date = article_parser.get_date()
-        self.text = article_parser.get_text()
+        self.year, self.edition = article_parser.get_year_edition()
+        self.url = article_parser.get_url()
+
+        try:
+            self.text = article_parser.get_text()
+        except:
+            self.text = None
+        
 
     def toJson(self):
         return self.__dict__
--- notes.md
+++ notes.md
@@ -1,9 +1,25 @@
-Web crawling: 
-- could use the scrapy package, with the CrawlSpider (it has rules for which links it can extract from the page and continue extracting) https://docs.scrapy.org/en/latest/topics/spiders.html
-
 article_scraper.py has the core classes to scrape the Qiushi articles, which have already been tested
 
 article_analysis.ipynb uses the article_scraper to get the articles and do some basic word count analysis and such
 
 Issues: 
 - some of the editions will basically have some doubled stuff, since they have an article that includes multiple articles, with links from the homepage to each of them as well, as seen here: http://www.qstheory.cn/dukan/qs/2014/2019-08/01/c_1124820140.htm 2019年第15期
+  - The comprehensive link and the sublinks all get scrapped from the main page
+- Author names
+  - Some of the names have spaces in them (if they only have two characters) - for the authors
+  - Some articles have multiple authors
+  - could I store the authors as a list? What do i want to use the authors for? Do I actually need them? Or do I want to see a change in authorship over time? Probably not...
+  - I could throw the authors at ChatGPT... maybe the rule to use is if there are any spaces? 
+  - first check if there are spaces, then if there is just one in the middle of two characters, remove it, and if not then throw it at ChatGPT?
+  - ChatGPT prompt: Please give me the names of the authors included in this text, and remove any unnecessary spaces within names. Please give me the names, seperated by a space, and nothing else. For example, if the text was "求是》杂志记者 何雯雯 新县融媒体中心记者 韩 燕", I would want you to tell me "何雯雯 韩燕". The text is: {author}
+  - that'll work 
+  - RESOLVED
+
+
+I am probably going to come up with additional queries that I want to do later... so I will need a way to do new analysis of the documents after they are stored
+
+MongoDB:
+- Use the calebh27@byu.edu login
+- how to iterate over each document? and keep track of the ones that have already been done? And run a function again, so that it only runs on ones that haven't already been done? 
+
+Next step: Add the articles to MongoDB, and then start practicing doing the operations on them
